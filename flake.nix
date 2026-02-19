08fc149631215edda97711604cc72551938059ce
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2026-02-19 15:18:51 -0300

    chore: formatting flake.nix

--- flake.nix
+++ flake.nix
@@ -1,33 +1,34 @@
 {
-  description = "Handmade Hero development environment";
+    description = "Handmade Hero development environment";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
 
-  outputs = { self, nixpkgs }:
+    outputs = { self, nixpkgs }:
     let
-      systems = [ "x86_64-linux" ];
-      forAllSystems = nixpkgs.lib.genAttrs systems;
+        systems = [ "x86_64-linux" ];
+        forAllSystems = nixpkgs.lib.genAttrs systems;
     in
     {
-      devShells = forAllSystems (system:
+        devShells = forAllSystems (system:
         let
-          pkgs = nixpkgs.legacyPackages.${system};
+            pkgs = nixpkgs.legacyPackages.${system};
         in
         {
-          default = pkgs.mkShell {
-            packages = [
-              pkgs.gcc
-              pkgs.clang-tools
-            ];
+            default = pkgs.mkShell {
+                packages = [
+                    pkgs.gcc # compiler
+                    pkgs.clang-tools # neovim LSP
+                ];
 
-            buildInputs = [
-              pkgs.libx11
-              pkgs.alsa-lib
-            ];
-          };
+                buildInputs = [
+                    pkgs.libx11
+                    pkgs.alsa-lib
+                    pkgs.SDL2
+                ];
+            };
         }
-      );
+        );
     };
 }
